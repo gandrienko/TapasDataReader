ead4e4c29c701c0b6aae1d5612d52d70be82b03c
natascha <natalia.andrienko@iais.fraunhofer.de>
2021-04-20 11:21:08 +0200

    reading solutions

--- src/TapasDataReader/Readers.java
+++ src/TapasDataReader/Readers.java
@@ -82,6 +82,9 @@ public class Readers {
   }
 
   public static Hashtable<String,Vector<Record>> readFlightPlans (String fname, Hashtable<String,Flight> flights) {
+    return readFlightPlans(fname,-1,flights);
+  }
+  public static Hashtable<String,Vector<Record>> readFlightPlans (String fname, int theStep, Hashtable<String,Flight> flights) {
     Hashtable<String,Vector<Record>> records=new Hashtable(100000);
     try {
       BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fname+".csv")))) ;
@@ -93,7 +96,7 @@ public class Readers {
             columnDelays=countCommas(header.substring(0,header.indexOf("Delays"))),
             columnSector=countCommas(header.substring(0,header.indexOf("Sector_0"))),
             columnEntryTime=countCommas(header.substring(0,header.indexOf("EntryTime_0")));
-        String[] columns=header.split(",");
+        //String[] columns=header.split(",");
         Flight flight=null;
         String flightID=null;
         while ((strLine = br.readLine()) != null) {
@@ -101,12 +104,15 @@ public class Readers {
           String[] tokens=str.split(",");
           String id=tokens[columnFlightID];
           if (!id.equals(flightID)) { // search in hashtable only if a new flight
-            flight=flights.get(id);
+            if (flights==null)
+              flight=null;
+            else
+              flight=flights.get(id);
             flightID=id;
             M++;
           }
           int delay=Integer.valueOf(tokens[columnDelays]);
-          if (delay<=flight.delays[flight.delays.length-1]) {
+          if ((flight==null && delay==0) || (flight!=null && delay<=flight.delays[flight.delays.length-1])) {
             //if (delay>0)
               //System.out.println("* delay="+delay);
             // 1. parse record into a sequence of sectors with times
@@ -133,8 +139,8 @@ public class Readers {
                 vr.add(r);
               }
             // 2. output for all steps with the same delay
-            for (int step = 0; step < flight.delays.length; step++)
-              if (flight.delays[step] == delay)
+            for (int step = 0; step < ((flight==null) ? 1 : flight.delays.length); step++)
+              if ((theStep==-1 || step==theStep) && (flight==null || flight.delays[step]==delay))
                 for (Record r:vr) {
                   //bw.write("FLIGHTID,STEP,DELAY,SECTOR,ENTRYTIME,EXITTIME,ENTRYTIMEN,EXITTIMEN,FROMSECTOR,TOSECTOR\n");
                   //bw.write(flight.id+","+step+","+delay+","+r.Sector+","+r.EntryTime+","+r.ExitTime+","+r.EntryTimeN+","+r.ExitTimeN+","+r.FromSector+","+r.ToSector+"\n");
@@ -162,6 +168,73 @@ public class Readers {
     return records;
   }
 
+  public static Hashtable<String,Vector<Record>> readSolutionAsStep (String fname, int theStep) {
+    Hashtable<String,Vector<Record>> records=new Hashtable(100000);
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fname+".csv")))) ;
+      String strLine;
+      int N=0, M=0, K=0;
+      try {
+        String header=br.readLine();
+        int columnFlightID=countCommas(header.substring(0,header.indexOf("FlightID"))),
+                columnDelays=countCommas(header.substring(0,header.indexOf("Delays"))),
+                columnSector=countCommas(header.substring(0,header.indexOf("Sector_0"))),
+                columnEntryTime=countCommas(header.substring(0,header.indexOf("EntryTime_0")));
+        //String[] columns=header.split(",");
+        //Flight flight=null;
+        //String flightID=null;
+        while ((strLine = br.readLine()) != null) {
+          String str=strLine.replaceAll(" ","");
+          String[] tokens=str.split(",");
+          String id=tokens[columnFlightID];
+          int delay=Integer.valueOf(tokens[columnDelays]);
+          Vector<Record> vr=new Vector<Record>(columnEntryTime-columnSector);
+          for (int i=columnSector; i<columnEntryTime; i++)
+            if (tokens[i].equals("NULL") || tokens[i].equals("NONE"))
+              ;
+            else {
+              Record r=new Record();
+              r.flight=id;
+              r.sector=tokens[i];
+              r.delay=delay;
+              if (i>columnSector)
+                r.FromS=tokens[i-1];
+              else
+                r.FromS="NULL";
+              if (i<columnEntryTime)
+                r.ToS=tokens[i+1];
+              else
+                r.ToS="NULL";
+              r.FromT=tokens[columnEntryTime+i-columnSector];
+              r.ToT=tokens[1+columnEntryTime+i-columnSector];
+              r.calc();
+              vr.add(r);
+            }
+            for (Record r:vr) {
+              //bw.write("FLIGHTID,STEP,DELAY,SECTOR,ENTRYTIME,EXITTIME,ENTRYTIMEN,EXITTIMEN,FROMSECTOR,TOSECTOR\n");
+              //bw.write(flight.id+","+step+","+delay+","+r.Sector+","+r.EntryTime+","+r.ExitTime+","+r.EntryTimeN+","+r.ExitTimeN+","+r.FromSector+","+r.ToSector+"\n");
+              K++;
+              Record rr=r.clone();
+              rr.step=theStep;
+              String key=rr.sector+"_"+rr.step;
+              Vector<Record> vrr=records.get(key);
+              if (vrr==null) {
+                vrr=new Vector<Record>(100);
+                records.put(key,vrr);
+              }
+              vrr.add(rr);
+            }
+          N++;
+          if (N % 10000 == 0)
+            System.out.println("* flights: "+M+" flights in "+N+" flightplans lines processed, "+K+" outputs recorded");
+        }
+        br.close();
+        //bw.close();
+        System.out.println("* flights: "+M+" flights in "+N+" flightplans lines processed, "+K+" outputs recorded");
+      } catch  (IOException io) {}
+    } catch (FileNotFoundException ex) {System.out.println("problem reading file "+fname+" : "+ex);}
+    return records;
+  }
 
   protected static int countCommas (String str) {
     int n=0;
