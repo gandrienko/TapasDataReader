04fce15195fbe0f24896fc14b422617180b40930
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2021-05-26 17:03:14 +0200

    rendering mode: time of day

--- src/TapasUtilities/RenderLabelBarChart.java
+++ src/TapasUtilities/RenderLabelBarChart.java
@@ -5,14 +5,19 @@ import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
 public class RenderLabelBarChart extends JLabel_BarChart implements TableCellRenderer {
+  boolean bModeTimeOfDay=false;
   public RenderLabelBarChart(float min, float max) {
     super(min,max);
     setOpaque(false);
   }
+  public void setbModeTimeOfDay() { bModeTimeOfDay=true; }
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
     int v=((Integer)value).intValue();
     setValue(v);
-    setText(""+v);
+    if (bModeTimeOfDay)
+      setText(String.format("%02d",v/60)+":"+String.format("%02d",v%60));
+    else
+      setText(""+v);
     if (isSelected)
       setBackground(table.getSelectionBackground());
     else
