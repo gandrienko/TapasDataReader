6d261191bddc66f0a58683f013ddfc781c8fba14
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2021-06-01 19:09:02 +0200

    JLabel for Value in SubInterval: empty rendering if value outside the range

--- src/TapasUtilities/JLabel_ValueInSubinterval.java
+++ src/TapasUtilities/JLabel_ValueInSubinterval.java
@@ -14,22 +14,27 @@ public class JLabel_ValueInSubinterval extends JLabel {
     this.global_max=v[2];
     this.int_min=v[3];
     this.int_max=v[4];
-    if (value==Math.round(value))
-      setText(Integer.valueOf((int)value).toString());
+    if (value<global_min || value>global_max)
+      setText("");
     else
-      setText(""+value);
+      if (value==Math.round(value))
+        setText(Integer.valueOf((int)value).toString());
+      else
+        setText(""+value);
   }
   public void paint (Graphics g) {
-    g.setColor(getBackground());
-    g.fillRect(0,0,getWidth(),getHeight());
-    int x1=(int)Math.round((int_min-global_min)*getWidth()/(global_max-global_min)),
-        x2=(int)Math.round((int_max-global_min)*getWidth()/(global_max-global_min)),
-        x=(int)Math.round((value-global_min)*getWidth()/(global_max-global_min));
-    g.setColor(Color.lightGray);
-    g.fillRect(x1,getHeight()/2,x2-x1,getHeight()/2);
-    g.setColor(Color.black);
-    g.drawLine(x,0,x,getHeight());
-    //System.out.println("* v="+value+" in ["+int_min+".."+int_max+"] in ["+global_min+".."+global_max+"], x="+x+" in ["+x1+".."+x2+"]; width="+getWidth());
+    if (value>=global_min && value<=global_max) {
+      g.setColor(getBackground());
+      g.fillRect(0, 0, getWidth(), getHeight());
+      int x1 = (int) Math.round((int_min - global_min) * getWidth() / (global_max - global_min)),
+              x2 = (int) Math.round((int_max - global_min) * getWidth() / (global_max - global_min)),
+              x = (int) Math.round((value - global_min) * getWidth() / (global_max - global_min));
+      g.setColor(Color.lightGray);
+      g.fillRect(x1, getHeight() / 2, x2 - x1, getHeight() / 2);
+      g.setColor(Color.black);
+      g.drawLine(x, 0, x, getHeight());
+      //System.out.println("* v="+value+" in ["+int_min+".."+int_max+"] in ["+global_min+".."+global_max+"], x="+x+" in ["+x1+".."+x2+"]; width="+getWidth());
+    }
     super.paint(g);
   }
 }
