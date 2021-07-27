70c9048fd41405b3869d3827ded79dc46e67a35c
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2021-07-27 16:43:31 +0200

    RenderBarChart for floats & doubles

--- src/TapasUtilities/RenderLabelBarChart.java
+++ src/TapasUtilities/RenderLabelBarChart.java
@@ -14,18 +14,28 @@ public class RenderLabelBarChart extends JLabel_BarChart implements TableCellRen
   }
   public void setbModeTimeOfDay() { bModeTimeOfDay=true; }
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-    if (value==null || ((Integer) value).intValue()<min || ((Integer) value).intValue()>max) {
+    Float v=Float.NaN;
+    if (value!=null) {
+      if (value instanceof Float)
+        v=(Float)value;
+      if (value instanceof Double)
+        v=((Double)value).floatValue();
+      if (value instanceof Integer)
+        v=((Integer)value).floatValue();
+    }
+    if (value==null || v<min || v>max) {
       setText("");
       setValue(min-1);
     }
-    else
-      {
-      int v = ((Integer) value).intValue();
+    else {
       setValue(v);
       if (bModeTimeOfDay)
         setText(String.format("%02d", v / 60) + ":" + String.format("%02d", v % 60));
       else
-        setText("" + v);
+        if (value instanceof Integer)
+          setText(""+((Integer)value));
+        else
+          setText(String.format("%.5f",v));
     }
     if (isSelected)
       setBackground(table.getSelectionBackground());
